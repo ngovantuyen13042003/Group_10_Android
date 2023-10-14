b0a7f41700420927045e50d891c8540506019da0
Ngo Van Tuyen <ngovantuyen13042003@gmail.com>
2023-10-14 17:19:49 +0700

    UI signup, signin, product

---
+++ MedicineDeliveryApp/app/src/main/java/com/example/medicinedeliveryapp/product_page.java
@@ -0,0 +1,13 @@
+package com.example.medicinedeliveryapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class product_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_page);
+    }
+}
