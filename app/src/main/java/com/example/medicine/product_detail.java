67be85d6950e737101995c5204e50173c1512c6e
Ngo Van Tuyen <ngovantuyen13042003@gmail.com>
2023-10-21 21:33:56 +0700

    footer, product

---
+++ app/src/main/java/com/example/medicine/product_detail.java
@@ -0,0 +1,14 @@
+package com.example.medicine;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class product_detail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_detail);
+    }
+}
