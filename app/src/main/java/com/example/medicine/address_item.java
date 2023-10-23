31eac0bc764c1e236739832bf50855ab8023945c
NHATTAN3043 <nhattan3043@gmail.com>
2023-10-24 00:47:49 +0700

    Tan push code duoc phan

---
+++ app/src/main/java/com/example/medicine/address_item.java
@@ -0,0 +1,17 @@
+package com.example.medicine;
+
+public class address_item {
+    private String diachi;
+
+    public address_item(String diachi) {
+        this.diachi = diachi;
+    }
+
+    public String getDiachi() {
+        return diachi;
+    }
+
+    public void setDiachi(String diachi) {
+        this.diachi = diachi;
+    }
+}
