ed6a57f8e9d878375a4d390d75c151eb3b6fdc05
Ngo Van Tuyen <ngovantuyen13042003@gmail.com>
2023-10-28 02:58:42 +0700

    update product, product detail

--- app/src/main/java/com/example/medicine/product.java
+++ app/src/main/java/com/example/medicine/product.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -19,18 +20,33 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
 public class product extends AppCompatActivity {
-    ImageView detail1, bannerproduct, giohang;
-    TextView cart1;
+    ImageView detail1,detail2,detail3,detail4,detail5,detail6,detail7,detail8, bannerproduct, giohang;
+    TextView cart1,cart2,cart3,cart4,cart5,cart6,cart7,cart8;
     NavigationView navigationView;
     DrawerLayout drwproduct;
+    Button btn_mua1,btn_mua2,btn_mua3,btn_mua4,btn_mua5,btn_mua6,btn_mua7,btn_mua8;
     ImageView  imvHome, imvProduct,imvDonHang, imvTinNhan, imvTaiKhoan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.product);
         detail1 = findViewById(R.id.productImage1);
+        detail2 = findViewById(R.id.productImage2);
+        detail3 = findViewById(R.id.productImage3);
+        detail4 = findViewById(R.id.productImage4);
+        detail5 = findViewById(R.id.productImage5);
+        detail6 = findViewById(R.id.productImage6);
+        detail7 = findViewById(R.id.productImage7);
+        detail8 = findViewById(R.id.productImage8);
         bannerproduct= findViewById(R.id.banerproduct);
-        cart1 = findViewById(R.id.cart);
+        cart1 = findViewById(R.id.cart1);
+        cart2 = findViewById(R.id.cart2);
+        cart3 = findViewById(R.id.cart3);
+        cart4 = findViewById(R.id.cart4);
+        cart5 = findViewById(R.id.cart5);
+        cart6 = findViewById(R.id.cart6);
+        cart7 = findViewById(R.id.cart7);
+        cart8 = findViewById(R.id.cart8);
         drwproduct=findViewById(R.id.drawerproduct);
         navigationView = (NavigationView) findViewById(R.id.navigation);
 
@@ -48,7 +64,91 @@ public class product extends AppCompatActivity {
             }
         });
 
+        cart2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(product.this, cart.class));
+            }
+        });
+        cart3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(product.this, cart.class));
+            }
+        });
+        cart4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(product.this, cart.class));
+            }
+        });
+        cart5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(product.this, cart.class));
+            }
+        });
+        cart6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(product.this, cart.class));
+            }
+        });
+        cart7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(product.this, cart.class));
+            }
+        });
+        cart8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(product.this, cart.class));
+            }
+        });
         detail1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(product.this, product_detail.class));
+            }
+        });
+        detail2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(product.this, product_detail.class));
+            }
+        });
+        detail3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(product.this, product_detail.class));
+            }
+        });
+        detail4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(product.this, product_detail.class));
+            }
+        });
+        detail5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(product.this, product_detail.class));
+            }
+        });
+        detail6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(product.this, product_detail.class));
+            }
+        });
+        detail7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(product.this, product_detail.class));
+            }
+        });
+        detail8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(product.this, product_detail.class));
@@ -67,44 +167,64 @@ public class product extends AppCompatActivity {
             }
         });
 
-
-//        BottomNavigationView bottomNavigation = findViewById(R.id.bottom_product);
-//
-//        bottomNavigation.setOnNavigationItemSelectedListener(item -> {
-//            Intent intent = null;
-//            item.setChecked(true);
-//            switch (item.getItemId()) {
-//                case R.id.home:
-//                    // Chuyển đến màn hình trang chủ (HomeActivity)
-//
-//                    intent = new Intent(this, Home.class);
-//                    break;
-//                case R.id.product:
-//                    // Chuyển đến màn hình sản phẩm (ProductActivity)
-//                    intent = new Intent(this, product.class);
-//                    break;
-//                case R.id.donhang:
-//                    // Chuyển đến màn hình đơn hàng (DonHangActivity)
-//                    intent = new Intent(this, MainActivity.class);
-//                    break;
-//                case R.id.chat:
-//                    // Chuyển đến màn hình chat (ChatActivity)
-//                    intent = new Intent(this, tin_nhan.class);
-//                    break;
-//                case R.id.account:
-//                    // Chuyển đến màn hình tài khoản (AccountActivity)
-//                    intent = new Intent(this, acccount.class);
-//                    break;
-//                default:
-//                    intent = null;
+//        btn_mua1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(product.this, cart.class));
 //            }
-//
-//            if (intent != null) {
-//                startActivity(intent);
-//            }
-//
-//            return true;
 //        });
+//
+//        btn_mua2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(product.this, cart.class));
+//            }
+//        });
+//        btn_mua3.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(product.this, signin.class));
+//            }
+//        });
+//        btn_mua4.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(product.this, signin.class));
+//            }
+//        });
+//        btn_mua5.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(product.this, signin.class));
+//            }
+//        });
+//        btn_mua6.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(product.this, signin.class));
+//            }
+//        });
+//        btn_mua7.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(product.this, signin.class));
+//            }
+//        });
+//        btn_mua8.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(product.this, signin.class));
+//            }
+//        });
+
+
+
+
+
+
+
+
+
         imvHome.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
