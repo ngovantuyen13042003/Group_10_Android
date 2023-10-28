6dc2a80f921fd746a1673e7120883a5d254c777e
Ngo Van Tuyen <ngovantuyen13042003@gmail.com>
2023-10-28 19:49:08 +0700

    update redirect page

--- app/src/main/java/com/example/medicine/signin.java
+++ app/src/main/java/com/example/medicine/signin.java
@@ -3,18 +3,34 @@ package com.example.medicine;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class signin extends AppCompatActivity {
-
-    TextView lk;
+    Button btn_signin;
+    TextView lk,forget_password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
         lk = findViewById(R.id.lksignup);
+        btn_signin = findViewById(R.id.btn_signin);
+        forget_password = findViewById(R.id.quenMK);
+
+        forget_password.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(signin.this, forgetpassword.class));
+            }
+        });
+        btn_signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(signin.this, cart.class));
+            }
+        });
         lk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
